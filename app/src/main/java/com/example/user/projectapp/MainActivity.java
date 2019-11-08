a1aa7a9e7d5322d717b1d211bdc2e232b9d28a6e
ExcellAutovista <auto.vista12@gmail.com>
2019-11-08 17:03:24 +0530

    add java code in main activity

--- app/src/main/java/com/example/user/projectapp/MainActivity.java
+++ app/src/main/java/com/example/user/projectapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.user.projectapp;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.name_et)
+    EditText name_et;
+
+    @BindView(R.id.Contactno_et)
+    EditText Contactno_et;
+
+    @BindView(R.id.submit_bt)
+    Button submit_bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ButterKnife.bind(this);
+
+
         //test App
         //test
         //test demo
@@ -17,5 +41,27 @@ public class MainActivity extends AppCompatActivity {
         //testdemo
         //tet
 
+    }
+
+    @OnClick(R.id.submit_bt)
+    public void submit(){
+
+        PackageManager pm=getPackageManager();
+        try {
+            Intent waIntent = new Intent(Intent.ACTION_SEND);
+            waIntent.setType("text/plain");
+            String text = "YOUR TEXT HERE";
+
+            PackageInfo info=pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+            //Check if package exists or not. If not then code
+            //in catch block will be called
+            waIntent.setPackage("com.whatsapp");
+            waIntent.putExtra(Intent.EXTRA_TEXT, text);
+            startActivity(Intent.createChooser(waIntent, "Share with"));
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "WhatsApp not Installed", Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
